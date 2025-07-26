ef8bbf6009162daccccdb413f5cc1d859fbaa2db
siusiusiu <study@163.com>
2025-07-26 12:56:50 +0800

    合并两个有序链表

---
+++ src/main/java/org/study/linkTable/MergeTwoSortedList.java
@@ -0,0 +1,10 @@
+package org.study.linkTable;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+}
